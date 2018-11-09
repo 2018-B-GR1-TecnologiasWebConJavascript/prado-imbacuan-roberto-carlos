0c6ec08f1dfafb61149f49c3f621879f9ace7a7c
RobertoPrado <roberto.prado@handytec.mobi>
2018-11-09 16:40:59 -0500

    subscribers

--- 02-typescript/02-observables.js
+++ 02-typescript/02-observables.js
@@ -1,7 +1,13 @@
 const rxjs = require('rxjs');
-const numeros$ = rxjs.of(1, 2, 3, 4, 5, 6);
+const map = require('rxjs/operators').map;
+const numeros$ = rxjs.of(1, "Adrian", true, { nombre: 'adsad' }, [1, 2, 3], new Date());
 console.log(numeros$);
 numeros$
+    .pipe(map((valorActual) => {
+    return {
+        data: valorActual
+    };
+}))
     .subscribe((ok) => {
     console.log('En ok', ok);
 }, (error) => {
--- 02-typescript/02-observables.ts
+++ 02-typescript/02-observables.ts
@@ -1,11 +1,20 @@
 declare var require: any;
 const rxjs = require('rxjs');
-
-const numeros$ = rxjs.of(1,2,3,4,5,6);
+const map = require('rxjs/operators').map;
+const numeros$ = rxjs.of(1,"Adrian", 1,1,1,true,{nombre: 'adsad'},[1,2,3], new Date());
 
 console.log(numeros$);
 
 numeros$
+    .pipe(
+        map(
+            (valorActual) => {
+                return {
+                    data: valorActual
+                }
+            }
+        )
+    )
     .subscribe((ok)=> {
         console.log('En ok',  ok);
     }, (error) => {
