0638d3c0f4780efd9aa00b4e1b394e0c2257b277
RobertoPrado <roberto.prado@handytec.mobi>
2018-11-12 17:55:43 -0500

    Crear proyecto angular

---
+++ 03-angular/videos/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+function DecoradorMetodo() {
+  console.log('test');
+}
+
+@Component({ // Decorador
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'videos';
+  @DecoradorMetodo()
+  metodo() {
+
+  }
+}
